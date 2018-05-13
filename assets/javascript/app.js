b901bc829683c560460ce01e9ede081da3f782a4
cronk <ChristineRonk@Stevens-MacBook-Pro.local>
2018-05-12 21:17:57 -0500

    html file, js and css started

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB1KAko0WIgaqwLbOu0xaMZjz6yJ1Iqw7U",
+    authDomain: "train-scheduler-79367.firebaseapp.com",
+    databaseURL: "https://train-scheduler-79367.firebaseio.com",
+    projectId: "train-scheduler-79367",
+    storageBucket: "",
+    messagingSenderId: "953141880946"
+  };
+  firebase.initializeApp(config);
+</script>
